95be22cef29cf608e5f00254df0ff647bb260951
Aditya Raje <160386539+Raje0906@users.noreply.github.com>
2025-03-19 18:21:55 +0530

    Add files via upload

---
+++ JS/variable/script.js
@@ -0,0 +1,15 @@
+let name = "Alice";
+const age = 25;
+var city = "New York";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+
+let str ="hello";
+let num =42;
+let bool=true;
+let undef;
+let nul=null;
+
+console.log(typeof str,typeof num, typeof bool, typeof undef,typeof null)
